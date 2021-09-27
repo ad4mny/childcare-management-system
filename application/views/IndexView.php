3f89fd374e5f049b1064e46dfd4355095ad9285b
adamz <58021562+ad4mny@users.noreply.github.com>
2021-09-27 21:17:06 +0800

    removed index view and controller

--- application/views/IndexView.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <div class="row position-absolute top-50 w-100">
-        <div class="col">
-            <h1 class="font-weight-light">Welcome to Bonda Sediah.</h1>
-            <p class="lead text-white">Your Childcare Management System.</p>
-            <a href="#" class="btn btn-outline-light">Register</a>
-        </div>
-    </div>
-</div>
