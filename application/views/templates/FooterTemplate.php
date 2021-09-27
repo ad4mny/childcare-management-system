745d0a50d7ba15586e44953a0e18ef42deda5193
adamz <58021562+ad4mny@users.noreply.github.com>
2021-09-27 12:37:40 +0800

    added navigation, header and footer templates

---
+++ application/views/templates/FooterTemplate.php
@@ -0,0 +1,11 @@
+
+<div class="d-none d-lg-block text-center py-2 fixed-bottom">
+    <small>&copy; Bonda Sediah 2021 </small>
+</div>
+
+
+<script src="<?php echo base_url(); ?>assets/js/jquery-3.6.0.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
---
+++ application/views/templates/NavigationTemplate.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-light bg-light justify-content-between">
+    <a class="navbar-brand">Bonda Sediah</a>
+    <form class="form-inline">
+        <button class="btn btn-outline-success" type="button">Login</button>
+        <button class="btn btn-outline-secondary" type="button">Register</button>
+    </form>
+</nav>
