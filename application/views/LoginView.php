b1162bda26790fbdc612947e203c38ef2d9e357c
adamz <58021562+ad4mny@users.noreply.github.com>
2021-09-28 02:32:04 +0800

    updated landing page background css image

--- application/views/LoginView.php
+++ application/views/LoginView.php
@@ -1,29 +1,30 @@
-<nav class="navbar navbar-light bg-light justify-content-between px-3">
-    <a class="navbar-brand">Bonda Sediah</a>
-</nav>
-
-<div class="container position-absolute top-50 start-50 translate-middle w-100 ">
-    <div class="row d-flex align-items-center">
-        <div class="col">
-            <h1 class="font-weight-light text-white">Welcome to Bonda Sediah</h1>
-            <p class="lead text-white">Your Childcare Management System. Register your child, today.</p>
-            <a href="<?php echo base_url() . 'register'; ?>" class="btn btn-outline-light">Register Now</a>
-        </div>
-        <div class="col-4 bg-white p-4 rounded-3">
-            <h3>Login</h3>
-            <form method="post" action="<?php echo base_url() . 'login/submit'; ?>">
-                <div class="form-group pb-2">
-                    <small>Username</small>
-                    <input type="text" class="form-control" name="username" placeholder="Enter your username">
-                </div>
-                <div class="form-group pb-2">
-                    <small>Password</small>
-                    <input type="password" class="form-control" name="password" placeholder="Enter your password">
-                </div>
-                <div class="form-group pb-2">
-                    <button type="submit" class="btn btn-primary" type="button" name="submit">Login</button>
-                </div>
-            </form>
+<div class="masthead">
+    <nav class="navbar navbar-light bg-light justify-content-between px-3">
+        <a class="navbar-brand">Bonda Sediah</a>
+    </nav>
+    <div class="container position-absolute top-50 start-50 translate-middle w-100">
+        <div class="row d-flex align-items-center">
+            <div class="col">
+                <h1 class="font-weight-light text-white">Welcome to Bonda Sediah</h1>
+                <p class="lead text-white">Your Childcare Management System. Register your child, today.</p>
+                <a href="<?php echo base_url() . 'register'; ?>" class="btn btn-outline-light">Register Now</a>
+            </div>
+            <div class="col-4 bg-white p-4 rounded-3">
+                <h3>Login</h3>
+                <form method="post" action="<?php echo base_url() . 'login/submit'; ?>">
+                    <div class="form-group pb-2">
+                        <small>Username</small>
+                        <input type="text" class="form-control" name="username" placeholder="Enter your username">
+                    </div>
+                    <div class="form-group pb-2">
+                        <small>Password</small>
+                        <input type="password" class="form-control" name="password" placeholder="Enter your password">
+                    </div>
+                    <div class="form-group pb-2">
+                        <button type="submit" class="btn btn-primary" type="button" name="submit">Login</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
--- application/views/RegisterView.php
+++ application/views/RegisterView.php
@@ -1,45 +1,48 @@
-<nav class="navbar navbar-light bg-light justify-content-between px-3">
-    <a class="navbar-brand">Bonda Sediah</a>
-    <a href="<?php echo base_url() . 'login'; ?>" class="btn btn-outline-primary">Login</a>
-</nav>
+<div class="masthead">
 
-<div class="container position-absolute top-50 start-50 translate-middle w-100 ">
-    <div class="row d-flex align-items-center">
-        <div class=" offset-2 col-8 bg-white p-4 rounded-3">
-            <h3>Registration</h3>
-            <form class="row g-2" method="post" action="<?php echo base_url() . 'register/submit'; ?>">
-                <div class="col-6 pb-2">
-                    <small>Full Name</small>
-                    <input type="text" class="form-control" name="fullname" placeholder="Enter your full name as in MyKad">
-                </div>
-                <div class="col-6 pb-2">
-                    <small>Indentification No. (MyKad)</small>
-                    <input type="text" class="form-control" name="icnumber" placeholder="Enter your identificaiton number">
-                </div>
-                <div class="col-6 pb-2">
-                    <small>Phone No. (Mobile)</small>
-                    <input type="text" class="form-control" name="phone" placeholder="Enter your mobile number">
-                </div>
-                <div class="col-6 pb-2">
-                    <small>Address</small>
-                   <textarea class="form-control" name="address" placeholder="Enter your full address" rows="3" max="150"></textarea>
-                </div>
-                <div class="col-6 pb-2">
-                    <small>Username</small>
-                    <input type="text" class="form-control" name="username" placeholder="Enter your username">
-                </div>
-                <div class="col-6 pb-2">
-                    <small>Password</small>
-                    <input type="password" class="form-control" name="password" placeholder="Enter your password">
-                </div>
-                <div class="col-6 offset-6 pb-2">
-                    <small>Confirm Password</small>
-                    <input type="password" class="form-control" name="confirmpassword" placeholder="Confirm your password">
-                </div>
-                <div class="col-12">
-                    <button type="submit" class="btn btn-primary" type="button" name="submit">Register</button>
-                </div>
-            </form>
+    <nav class="navbar navbar-light bg-light justify-content-between px-3">
+        <a class="navbar-brand">Bonda Sediah</a>
+        <a href="<?php echo base_url() . 'login'; ?>" class="btn btn-outline-primary">Login</a>
+    </nav>
+
+    <div class="container position-absolute top-50 start-50 translate-middle w-100 ">
+        <div class="row d-flex align-items-center">
+            <div class=" offset-2 col-8 bg-white p-4 rounded-3">
+                <h3>Registration</h3>
+                <form class="row g-2" method="post" action="<?php echo base_url() . 'register/submit'; ?>">
+                    <div class="col-6 pb-2">
+                        <small>Full Name</small>
+                        <input type="text" class="form-control" name="fullname" placeholder="Enter your full name as in MyKad">
+                    </div>
+                    <div class="col-6 pb-2">
+                        <small>Indentification No. (MyKad)</small>
+                        <input type="text" class="form-control" name="icnumber" placeholder="Enter your identificaiton number">
+                    </div>
+                    <div class="col-6 pb-2">
+                        <small>Phone No. (Mobile)</small>
+                        <input type="text" class="form-control" name="phone" placeholder="Enter your mobile number">
+                    </div>
+                    <div class="col-6 pb-2">
+                        <small>Address</small>
+                        <textarea class="form-control" name="address" placeholder="Enter your full address" rows="3" max="150"></textarea>
+                    </div>
+                    <div class="col-6 pb-2">
+                        <small>Username</small>
+                        <input type="text" class="form-control" name="username" placeholder="Enter your username">
+                    </div>
+                    <div class="col-6 pb-2">
+                        <small>Password</small>
+                        <input type="password" class="form-control" name="password" placeholder="Enter your password">
+                    </div>
+                    <div class="col-6 offset-6 pb-2">
+                        <small>Confirm Password</small>
+                        <input type="password" class="form-control" name="confirmpassword" placeholder="Confirm your password">
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary" type="button" name="submit">Register</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
