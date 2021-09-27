1bc237afc6064358e1f52ab0ac0673c860d57250
adamz <58021562+ad4mny@users.noreply.github.com>
2021-09-27 21:17:24 +0800

    temporarily removed navigation

--- application/views/templates/NavigationTemplate.php
+++
@@ -1,7 +0,0 @@
-<nav class="navbar navbar-light bg-light justify-content-between">
-    <a class="navbar-brand">Bonda Sediah</a>
-    <form class="form-inline">
-        <button class="btn btn-outline-success" type="button">Login</button>
-        <button class="btn btn-outline-secondary" type="button">Register</button>
-    </form>
-</nav>
